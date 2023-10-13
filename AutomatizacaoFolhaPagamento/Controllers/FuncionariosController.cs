1a49d6b4fb34c0a9591fda3d2adfa912b013c70b
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2023-10-13 00:36:48 -0300

    Cadastro de funcionários ajustado e sendo realizado no backend.

--- AutomatizacaoFolhaPagamento/Controllers/FuncionariosController.cs
+++ AutomatizacaoFolhaPagamento/Controllers/FuncionariosController.cs
@@ -1,6 +1,8 @@
 ﻿using AutomacaoFolhaPagamento.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
 using System.Text.Json;
 
 namespace AutomacaoFolhaPagamento.Controllers
@@ -33,6 +35,69 @@ namespace AutomacaoFolhaPagamento.Controllers
                     Value = d.id_cargo.ToString(),
                     Text = $"{d.id_cargo} - {d.nome_cargo}"
                 }).ToList();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Cadastro(NovoFuncionarioViewModel fun)
+        {
+            try
+            {
+                var client = _clientFactory.CreateClient();
+
+                var data = new
+                {
+                    nome = fun.nome,
+                    cpf = fun.cpf,
+                    sexo = fun.sexo ,
+                    cargo_id = fun.cargo_id,
+                    data_contratacao = fun.data_contratacao, 
+                    estado_civil = fun.estado_civil,
+                    rua = fun.rua,
+                    tipo_endereco = fun.tipo_endereco, 
+                    num_endereco =  fun.num_endereco,
+                    bairro = fun.bairro,
+                    cep = fun.cep,
+                    cidade = fun.cidade,
+                    uf_estado = fun.uf_estado,
+                    tipo_telefone = fun.tipo_telefone,
+                    numero_contato = fun.numero_contato
+                };
+
+                var content = new StringContent(JsonSerializer.Serialize(data), Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync("https://localhost:7067/api/Funcionarios/novoFuncionario", content);
+                if (response.StatusCode == System.Net.HttpStatusCode.Created)
+                {
+                    ViewData["SuccessMessage"] = "Cadastro realizado com sucesso!";
+                    await LoadCargos();
+                    ModelState.Clear();
+                    return View("Index", new NovoFuncionarioViewModel());
+                }
+                else if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                {
+                    ViewData["ErrorMessage"] = "Funcionário já cadastrado.";
+                    ModelState.Clear();
+                    await LoadCargos();
+                    return View("Index", new NovoFuncionarioViewModel());
+                }
+                else if (response.StatusCode == System.Net.HttpStatusCode.InternalServerError)
+                {
+
+                    ViewData["ErrorMessage"] = "Ocorreu um erro ao cadastrar o funcionário. Por favor, tente novamente.";
+                    ModelState.Clear();
+                    await LoadCargos();
+                    return View("Index", new NovoFuncionarioViewModel());
+                }
+
+                throw new Exception("Erro no servidor.");
+            }
+            catch
+            {
+                ViewData["ErrorMessage"] = "Não foi possível completar o cadastro.";
+                ModelState.Clear();
+                await LoadCargos();
+                return View("Index", new NovoFuncionarioViewModel());
             }
         }
 
---
+++ AutomatizacaoFolhaPagamento/Models/NovoFuncionarioViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace AutomacaoFolhaPagamento.Models
+{
+    public class NovoFuncionarioViewModel
+    {
+        public string nome { get; set; }
+        public string? cpf { get; set; }
+        public string? sexo { get; set; }
+        public int? cargo_id { get; set; }
+        public DateTime? data_contratacao { get; set; }
+        public string? estado_civil { get; set; }
+        public string? rua { get; set; }
+        public string? tipo_endereco { get; set; }
+        public string? num_endereco { get; set; }
+        public string? bairro { get; set; }
+        public string? cep { get; set; }
+        public string? cidade { get; set; }
+        public string? uf_estado { get; set; }
+        public string? tipo_telefone { get; set; }
+        public string? numero_contato { get; set; }
+    }
+}
